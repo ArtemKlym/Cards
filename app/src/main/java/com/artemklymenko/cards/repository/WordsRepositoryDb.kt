b0e2338dfa5c646ac177e4f832952540e5220752
Artem <artemklymenko892@gmail.com>
2023-10-09 16:58:48 +0300

    Created package repository. Implemented WordsRepositoryDb

---
+++ app/src/main/java/com/artemklymenko/cards/repository/WordsRepositoryDb.kt
@@ -0,0 +1,19 @@
+package com.artemklymenko.cards.repository
+
+import com.artemklymenko.cards.db.WordDao
+import com.artemklymenko.cards.db.Words
+import javax.inject.Inject
+
+class WordsRepositoryDb @Inject constructor(
+    private val dao: WordDao
+) {
+    suspend fun  insertWords(words: Words) = dao.insertWords(words)
+
+    suspend fun updateWords(words: Words) = dao.updateWords(words)
+
+    suspend fun deleteWords(words: Words) = dao.deleteWords(words)
+
+    suspend fun getAllWords() = dao.getAllWords()
+
+    suspend fun getWords(wordsId: Int) = dao.getWords(wordsId)
+}
