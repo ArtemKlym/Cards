c64aa3510c0639a06042b0323138725e783d29be
Artem <artemklymenko892@gmail.com>
2023-10-16 17:08:24 +0300

    Created AddWordsActivity

---
+++ app/src/main/java/com/artemklymenko/cards/view/activities/AddWordsActivity.kt
@@ -0,0 +1,12 @@
+package com.artemklymenko.cards.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artemklymenko.cards.R
+
+class AddWordsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_words)
+    }
+}
