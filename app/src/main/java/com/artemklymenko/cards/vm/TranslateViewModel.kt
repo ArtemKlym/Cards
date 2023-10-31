1899168c1293c3d9e64231ed877cf79e2bd50784
Artem <artemklymenko892@gmail.com>
2023-10-31 18:09:59 +0200

    Implemented TranslateViewModel

--- app/src/main/java/com/artemklymenko/cards/vm/TranslateViewModel.kt
+++ app/src/main/java/com/artemklymenko/cards/vm/TranslateViewModel.kt
@@ -2,26 +2,12 @@ package com.artemklymenko.cards.vm
 
 import android.app.Application
 import android.content.Context
-import android.util.LruCache
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import com.artemklymenko.cards.R
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.tasks.Tasks
 import com.google.mlkit.common.model.DownloadConditions
-import com.google.mlkit.common.model.RemoteModelManager
-import com.google.mlkit.nl.translate.TranslateLanguage
-import com.google.mlkit.nl.translate.TranslateRemoteModel
 import com.google.mlkit.nl.translate.Translation
-import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
-import dagger.hilt.android.internal.Contexts.getApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,4 +15,45 @@ class TranslateViewModel @Inject constructor(
     @ApplicationContext private val context: Context
 ) : AndroidViewModel(context as Application){
 
+    private var sourceLangCode = "en"
+    private var targetLangCode = "uk"
+
+    fun setSourceLanguage(languageCode: String) {
+        sourceLangCode = languageCode
+    }
+
+    fun setTargetLanguage(languageCode: String) {
+        targetLangCode = languageCode
+    }
+    
+   fun translateText(
+        sourceText: String,
+        onTranslationResult: (String) -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        val translatorOptions = TranslatorOptions.Builder()
+            .setSourceLanguage(sourceLangCode)
+            .setTargetLanguage(targetLangCode)
+            .build()
+
+        val translator = Translation.getClient(translatorOptions)
+
+        val downloadConditions = DownloadConditions.Builder()
+            .requireWifi()
+            .build()
+
+        translator.downloadModelIfNeeded(downloadConditions)
+            .addOnSuccessListener {
+                translator.translate(sourceText)
+                    .addOnSuccessListener {
+                        onTranslationResult(it)
+                    }
+                    .addOnFailureListener { e ->
+                        onError(e)
+                    }
+            }
+            .addOnFailureListener { e ->
+                onError(e)
+            }
+    }
 }
