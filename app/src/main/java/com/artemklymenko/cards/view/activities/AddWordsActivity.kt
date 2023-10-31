dfe4f035ac25875b61d9ede6c5f3c7095fb905fa
Artem <artemklymenko892@gmail.com>
2023-10-31 18:16:03 +0200

    Reworked AddWordsActivity

--- app/src/main/java/com/artemklymenko/cards/view/activities/AddWordsActivity.kt
+++ app/src/main/java/com/artemklymenko/cards/view/activities/AddWordsActivity.kt
@@ -1,36 +1,28 @@
 package com.artemklymenko.cards.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.Menu
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.PopupMenu
 import android.widget.Spinner
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.ViewModelProvider
-import com.artemklymenko.cards.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.artemklymenko.cards.databinding.ActivityAddWordsBinding
 import com.artemklymenko.cards.di.ModelLanguage
 import com.artemklymenko.cards.vm.TranslateViewModel
 import com.google.android.material.textfield.TextInputEditText
-import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
-import com.google.mlkit.nl.translate.Translation
-import com.google.mlkit.nl.translate.Translator
-import com.google.mlkit.nl.translate.TranslatorOptions
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Locale
 
 @AndroidEntryPoint
 class AddWordsActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAddWordsBinding
     private lateinit var sourceLang: TextInputEditText
     private lateinit var targetLang: TextInputEditText
@@ -38,15 +30,8 @@ class AddWordsActivity : AppCompatActivity() {
     private lateinit var spinnerTargetLang: Spinner
     private lateinit var translateBtn: Button
 
-    private var languageArrayList: ArrayList<ModelLanguage>? = null
-
-    private lateinit var translatorOptions: TranslatorOptions
-    private lateinit var translator: Translator
-
-    private var sourceLangCode = "en"
-    private var targetLangCode = "uk"
-    private var sourceLangTitle = "English"
-    private var targetLangTitle = "Ukrainian"
+    private var languageArrayList: List<ModelLanguage> = emptyList()
+    private val translationViewModel: TranslateViewModel by viewModels()
 
     companion object {
         private const val TAG = "MAIN_TAG"
@@ -59,125 +44,84 @@ class AddWordsActivity : AppCompatActivity() {
 
         loadAvailableLanguages()
 
-        sourceLang = binding.etOrigin
-        targetLang = binding.etTranslated
-
-        spinnerSourceLang = binding.spOrigin
-        spinnerTargetLang = binding.spTranslated
-
-        translateBtn = binding.btnAdd
-
-        val languageAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, languageArrayList!!.map { it.languageTitle })
-        spinnerTargetLang.adapter = languageAdapter
-        spinnerSourceLang.adapter = languageAdapter
+        setupViews()
+        setupSpinners()
 
         spinnerSourceLang.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, i: Int, l: Long) {
-                sourceLanguageChoose(i)
+                val selectedLanguage = languageArrayList[i]
+                translationViewModel.setSourceLanguage(selectedLanguage.languageCode)
             }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                // Handle nothing selected if needed
-            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
 
         spinnerTargetLang.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, i: Int, l: Long) {
-                targetLanguageChoose(i)
+                val selectedLanguage = languageArrayList[i]
+                translationViewModel.setTargetLanguage(selectedLanguage.languageCode)
+                if(sourceLang.text!!.isNotEmpty()){
+                    validateData()
+                }
             }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                // Handle nothing selected if needed
-            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
 
         sourceLang.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // Not needed
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // Not needed
-            }
-
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun afterTextChanged(p0: Editable?) {
-                Log.d(TAG, "afterTextChanged called")
                 validateData()
             }
         })
+
+        translateBtn.setOnClickListener {
+
+        }
+    }
+
+    private fun setupViews() {
+        sourceLang = binding.etOrigin
+        targetLang = binding.etTranslated
+        spinnerSourceLang = binding.spOrigin
+        spinnerTargetLang = binding.spTranslated
+        translateBtn = binding.btnAdd
+    }
+
+    private fun setupSpinners() {
+        val languageAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, languageArrayList.map { it.languageTitle })
+        spinnerSourceLang.adapter = languageAdapter
+        spinnerTargetLang.adapter = languageAdapter
     }
 
     private fun validateData() {
         val sourceLangText = sourceLang.text.toString().trim()
-        Log.d(TAG, "validateData: sourceLangText: $sourceLangText")
-
         if (sourceLangText.isEmpty()) {
             binding.tilOrigin.error = "Enter text to translate"
         } else {
-            binding.tilOrigin.error = null
             startTranslation()
+            binding.tilOrigin.error = null
         }
     }
 
     private fun startTranslation() {
-        translatorOptions = TranslatorOptions.Builder()
-            .setSourceLanguage(sourceLangCode)
-            .setTargetLanguage(targetLangCode)
-            .build()
-        translator = Translation.getClient(translatorOptions)
-
-        val downloadConditions = DownloadConditions.Builder()
-            .requireWifi()
-            .build()
-
-        translator.downloadModelIfNeeded(downloadConditions)
-            .addOnSuccessListener {
-                Log.d(TAG, "startTranslation: translation model ready, start translation...")
-                binding.tvModel.text = "Downloading model..."
-                translator.translate(sourceLang.text.toString())
-                    .addOnSuccessListener {
-                        binding.tvModel.text = "Translating..."
-                        targetLang.setText(it)
-                    }
-                    .addOnFailureListener { e ->
-                        Log.d(TAG, "startTranslation: $e")
-                        Toast.makeText(this, "Failed to translate due to: $e", Toast.LENGTH_SHORT).show()
-                    }
-            }
-            .addOnFailureListener {
-                // Handle model download failure if needed
-            }
+        val sourceText = sourceLang.text.toString()
+        translationViewModel.translateText(sourceText, { translatedText ->
+            binding.tvModel.text = "Translating..."
+            targetLang.setText(translatedText)
+            translateBtn.isEnabled = sourceLang.text!!.isNotEmpty() && targetLang.text!!.isNotEmpty()
+        }, { e ->
+            Log.d(TAG, "startTranslation: $e")
+            Toast.makeText(this, "Failed to translate due to: $e", Toast.LENGTH_SHORT).show()
+        })
     }
 
     private fun loadAvailableLanguages() {
-        languageArrayList = ArrayList()
-
         val languageCodeList = TranslateLanguage.getAllLanguages()
-
-        for (languageCode in languageCodeList) {
+        languageArrayList = languageCodeList.map { languageCode ->
             val languageTitle = Locale(languageCode).displayLanguage
-
-            Log.d(TAG, "loadAvailableLanguages: languageCode: $languageCode")
-            Log.d(TAG, "loadAvailableLanguages: languageTitle: $languageTitle")
-
-            val modelLanguage = ModelLanguage(languageCode, languageTitle)
-            languageArrayList!!.add(modelLanguage)
+            ModelLanguage(languageCode, languageTitle)
         }
-    }
-
-    private fun sourceLanguageChoose(position: Int) {
-        val selectedLanguage = languageArrayList!![position]
-        sourceLangCode = selectedLanguage.languageCode
-        sourceLangTitle = selectedLanguage.languageTitle
-        Log.d(TAG, "sourceLanguageChoose: sourceLanguageCode: $sourceLangCode")
-        Log.d(TAG, "sourceLanguageChoose: sourceLanguageTitle: $sourceLangTitle")
-    }
-
-    private fun targetLanguageChoose(position: Int) {
-        val selectedLanguage = languageArrayList!![position]
-        targetLangCode = selectedLanguage.languageCode
-        targetLangTitle = selectedLanguage.languageTitle
-        Log.d(TAG, "targetLanguageChoose: targetLangCode: $targetLangCode")
-        Log.d(TAG, "targetLanguageChoose: targetLangTitle: $targetLangTitle")
     }
 }
