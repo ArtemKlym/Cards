43c5e0a0ba92b795f8c7b05f3da9575495058e8e
Artem <artemklymenko892@gmail.com>
2023-11-01 18:02:17 +0200

    Add CardsFragment

--- app/src/main/java/com/artemklymenko/cards/view/fragments/CardsFragment.kt
+++ app/src/main/java/com/artemklymenko/cards/view/fragments/CardsFragment.kt
@@ -5,56 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.artemklymenko.cards.R
+import com.artemklymenko.cards.adaptar.WordsAdapter
+import com.artemklymenko.cards.databinding.FragmentCardsBinding
+import com.artemklymenko.cards.vm.WordsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.FragmentScoped
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CardsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class CardsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentCardsBinding
+    private val viewModel: WordsViewModel by viewModels()
+
+    @Inject
+    lateinit var wordsAdapter: WordsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cards, container, false)
+        binding = FragmentCardsBinding.inflate(inflater, container, false)
+
+        checkItems()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CardsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CardsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun checkItems() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val list = viewModel.getAllWords()
+            CoroutineScope(Dispatchers.Main).launch {
+                wordsAdapter.differ.submitList(list)
+                setupRecyclerView()
             }
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerViewWords.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = wordsAdapter
+        }
+    }
+
+    companion object{
+
+        @JvmStatic
+        fun newInstance() = CardsFragment()
     }
 }
