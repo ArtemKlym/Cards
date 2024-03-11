264ffe8a3953fbc756b2927882ea97e60522b8a4
Artem <artemklymenko892@gmail.com>
2024-03-11 16:46:59 +0200

    Added new constant values

--- app/src/main/java/com/artemklymenko/cards/utils/Constants.kt
+++ app/src/main/java/com/artemklymenko/cards/utils/Constants.kt
@@ -3,4 +3,7 @@ package com.artemklymenko.cards.utils
 object Constants {
     const val TABLE_NAME = "table_words"
     const val DATABASE_NAME = "words.db"
+    const val TAG_REMINDER_WORKER = "reminder_worker"
+    const val TAG_WORK_MANAGER = "work_manager"
+    const val CHANNEL_ID = "transactions_reminder_channel"
 }
