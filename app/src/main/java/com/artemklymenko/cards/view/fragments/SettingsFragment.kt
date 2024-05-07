7d0517f4802661ce9dacf7bff1e3886941940951
Artem <artemklymenko892@gmail.com>
2024-05-07 07:12:33 +0300

    Implemented SettingsFragment

--- app/src/main/java/com/artemklymenko/cards/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/artemklymenko/cards/view/fragments/SettingsFragment.kt
@@ -1,60 +1,69 @@
 package com.artemklymenko.cards.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.artemklymenko.cards.R
+import com.artemklymenko.cards.databinding.FragmentSettingsBinding
+import com.artemklymenko.cards.notification.utils.setupNotificationsSwitch
+import com.artemklymenko.cards.vm.DataStorePreferenceManager
+import com.artemklymenko.cards.vm.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<LoginViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val dataStorePreferenceManager = DataStorePreferenceManager.getInstance(container!!.context)
+        binding.switchNotification.isChecked = dataStorePreferenceManager.notice
+
+        if (viewModel.currentUser?.email != null) {
+            binding.tvUserEmail.text = viewModel.currentUser!!.email
+        } else {
+            binding.tvUserEmail.text = getString(R.string.user)
+            }
+
+        binding.ivLogOut.setOnClickListener {
+            viewModel.logout()
+            switchToSignInFragment()
+        }
+
+        binding.switchNotification.setOnCheckedChangeListener { _, isChecked ->
+            setupNotificationsSwitch(
+                dataStorePreferenceManager,
+                isChecked,
+                container.context
+            )
+        }
+
+        return binding.root
+    }
+
+    private fun switchToSignInFragment() {
+        val signInFragment = SignInFragment.newInstance()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, signInFragment)
+            .commit()
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = SettingsFragment()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
