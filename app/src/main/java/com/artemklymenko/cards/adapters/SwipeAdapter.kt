810f7d4a420b8af321adea13753c061b08333f3f
Artem <artemklymenko892@gmail.com>
2024-06-13 06:07:25 +0300

    Implement multilingual text-to-speech functionality

--- app/src/main/java/com/artemklymenko/cards/adapters/SwipeAdapter.kt
+++ app/src/main/java/com/artemklymenko/cards/adapters/SwipeAdapter.kt
@@ -11,6 +11,7 @@ import android.widget.ImageButton
 import android.widget.TextView
 import com.artemklymenko.cards.R
 import com.artemklymenko.cards.db.Words
+import com.wajahatkarim3.easyflipview.EasyFlipView
 import java.util.Locale
 
 class SwipeAdapter(
@@ -27,6 +28,9 @@ class SwipeAdapter(
     private lateinit var frontText: TextView
     private lateinit var backText: TextView
     private lateinit var btnListen: ImageButton
+    private lateinit var flipView: EasyFlipView
+
+    private var origin = true
 
     override fun getCount(): Int = listWords.size
 
@@ -47,21 +51,38 @@ class SwipeAdapter(
 
         tts = TextToSpeech(parent!!.context,this)
 
+        flipView.setOnFlipListener { easyFlipView, _ ->
+            origin = easyFlipView.isFrontSide
+        }
+
         btnListen.setOnClickListener {
-            speakOut(currentWord.origin)
+            if(origin){
+                speakOut(currentWord.origin, currentWord.sourceLangCode)
+            }else{
+                speakOut(currentWord.translated, currentWord.targetLangCode)
+            }
+            origin = true
         }
 
         return initConvertView
     }
 
-    private fun speakOut(text: String) {
-        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    private fun speakOut(text: String, languageCode: String) {
+        val locale = getLocale(languageCode)
+        val result = tts!!.setLanguage(locale)
+
+        if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            Log.e("TTS", "The Language $languageCode is not supported!")
+        } else {
+            tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+        }
     }
 
     private fun initView(initConvertView: View) {
         frontText = initConvertView.findViewById(R.id.tvCardOriginal)
         backText = initConvertView.findViewById(R.id.tvCardTranslated)
         btnListen = initConvertView.findViewById(R.id.ibListen)
+        flipView = initConvertView.findViewById(R.id.easyFlipView)
     }
 
     fun addSwiped(words: Words) {
@@ -69,15 +90,70 @@ class SwipeAdapter(
         notifyDataSetChanged()
     }
 
-    override fun onInit(status: Int) {
-        if (status == TextToSpeech.SUCCESS) {
-            val result = tts!!.setLanguage(Locale.US)
+    override fun onInit(status: Int) {}
 
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS","The Language not supported!")
-            } else {
-                btnListen.isEnabled = true
-            }
+    private fun getLocale(languageCode: String): Locale {
+        return when (languageCode) {
+            "af" -> Locale("af")
+            "ar" -> Locale("ar")
+            "be" -> Locale("be")
+            "bg" -> Locale("bg")
+            "bn" -> Locale("bn")
+            "ca" -> Locale("ca")
+            "cs" -> Locale("cs")
+            "cy" -> Locale("cy")
+            "da" -> Locale("da")
+            "de" -> Locale.GERMANY
+            "el" -> Locale("el")
+            "en" -> Locale.ENGLISH
+            "eo" -> Locale("eo")
+            "es" -> Locale("es", "ES")
+            "et" -> Locale("et")
+            "fa" -> Locale("fa")
+            "fi" -> Locale("fi")
+            "fr" -> Locale.FRANCE
+            "ga" -> Locale("ga")
+            "gl" -> Locale("gl")
+            "gu" -> Locale("gu")
+            "he" -> Locale("he")
+            "hi" -> Locale("hi")
+            "hr" -> Locale("hr")
+            "ht" -> Locale("ht")
+            "hu" -> Locale("hu")
+            "id" -> Locale("id")
+            "is" -> Locale("is")
+            "it" -> Locale.ITALY
+            "ja" -> Locale.JAPAN
+            "ka" -> Locale("ka")
+            "kn" -> Locale("kn")
+            "ko" -> Locale.KOREA
+            "lt" -> Locale("lt")
+            "lv" -> Locale("lv")
+            "mk" -> Locale("mk")
+            "mr" -> Locale("mr")
+            "ms" -> Locale("ms")
+            "mt" -> Locale("mt")
+            "nl" -> Locale("nl")
+            "no" -> Locale("no")
+            "pl" -> Locale("pl")
+            "pt" -> Locale("pt")
+            "ro" -> Locale("ro")
+            "ru" -> Locale("ru")
+            "sk" -> Locale("sk")
+            "sl" -> Locale("sl")
+            "sq" -> Locale("sq")
+            "sv" -> Locale("sv")
+            "sw" -> Locale("sw")
+            "ta" -> Locale("ta")
+            "te" -> Locale("te")
+            "th" -> Locale("th")
+            "tl" -> Locale("tl")
+            "tr" -> Locale("tr")
+            "uk" -> Locale("uk")
+            "ur" -> Locale("ur")
+            "vi" -> Locale("vi")
+            "zh" -> Locale.CHINA
+            else -> Locale.ENGLISH
         }
     }
 }
