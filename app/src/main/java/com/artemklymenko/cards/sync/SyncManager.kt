62a176c4f9ee0cb57a99975c48a82dbef88cdf9b
Artem <artemklymenko892@gmail.com>
2024-06-14 20:20:10 +0300

    Add SyncManager

---
+++ app/src/main/java/com/artemklymenko/cards/sync/SyncManager.kt
@@ -0,0 +1,74 @@
+package com.artemklymenko.cards.sync
+
+import android.content.Context
+import com.artemklymenko.cards.db.Words
+import com.artemklymenko.cards.db.WordsRepositoryDb
+import com.artemklymenko.cards.firestore.model.Response
+import com.artemklymenko.cards.firestore.repository.CardsRepository
+import com.artemklymenko.cards.vm.DataStorePreferenceManager
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class SyncManager @Inject constructor(
+    private val firestoreRepository: CardsRepository,
+    private val wordsRepository: WordsRepositoryDb,
+    private val appContext: Context,
+) {
+    suspend fun synchronizeData(userId: String): Boolean {
+        val dataStorePreference = DataStorePreferenceManager.getInstance(appContext)
+        val currentTime = System.currentTimeMillis()
+        val lastSyncTime = dataStorePreference.lastSyncTime
+        val oneDayInMillis = TimeUnit.DAYS.toMillis(1)
+        return if (currentTime - lastSyncTime >= oneDayInMillis) {
+            val localCards = wordsRepository.getAllWords()
+            val firestoreCards = getFirestoreCardsAsCardsList(userId)
+            // Synchronize data
+            synchronizeWithFirestore(localCards, firestoreCards, userId)
+            dataStorePreference.lastSyncTime = currentTime
+            true
+        } else {
+            false
+        }
+    }
+
+    private suspend fun synchronizeWithFirestore(
+        localCards: List<Words>,
+        firestoreCards: List<Words>,
+        userId: String
+    ) {
+        if (firestoreCards.isEmpty() && localCards.isNotEmpty()) {
+            // Firestore is empty, upload all local data to Firestore
+            localCards.forEach { card ->
+                val newSidResponse = firestoreRepository.addCardToFirestore(userId, card)
+                if (newSidResponse is Response.Success) {
+                    val newSid = newSidResponse.data
+                    firestoreRepository.updateCardSid(userId, card.wordsId, newSid)
+                    wordsRepository.updateWordsSid(card.wordsId, newSid)
+                }
+            }
+        } else {
+            // Check for new or updated data in Firestore
+            firestoreCards.forEach { firestoreCard ->
+                val localCard = localCards.find { it.sid == firestoreCard.sid }
+                if (localCard == null || localCard.sync < firestoreCard.sync) {
+                    wordsRepository.insertWords(firestoreCard)
+                }
+            }
+
+            // After inserting new data into Firestore, update local objects with server IDs (sids)
+            val unsyncedCards = wordsRepository.getUnsyncedWords()
+            unsyncedCards.forEach { card ->
+                val newSidResponse = firestoreRepository.addCardToFirestore(userId, card)
+                if (newSidResponse is Response.Success) {
+                    val newSid = newSidResponse.data
+                    firestoreRepository.updateCardSid(userId, card.wordsId, newSid)
+                    wordsRepository.updateWordsSid(card.wordsId, newSid)
+                }
+            }
+        }
+    }
+
+    private suspend fun getFirestoreCardsAsCardsList(userId: String): List<Words> {
+        return firestoreRepository.getCardsFromFirestore(userId)
+    }
+}
