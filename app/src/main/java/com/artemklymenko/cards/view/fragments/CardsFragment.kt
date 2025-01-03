d4013840c11b48b76338d4b3ff418cf6297588f1
Artem <artemklymenko892@gmail.com>
2025-01-03 16:18:54 +0200

    Navigate to UpdateCardFragment on item click with bundled arguments

--- app/src/main/java/com/artemklymenko/cards/view/fragments/CardsFragment.kt
+++ app/src/main/java/com/artemklymenko/cards/view/fragments/CardsFragment.kt
@@ -8,6 +8,7 @@ import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.artemklymenko.cards.R
 import com.artemklymenko.cards.adapters.WordsAdapter
 import com.artemklymenko.cards.databinding.FragmentCardsBinding
 import com.artemklymenko.cards.db.Words
@@ -16,7 +17,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class CardsFragment : Fragment() {
@@ -25,9 +25,7 @@ class CardsFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel: WordsViewModel by viewModels()
     private lateinit var listWords:List<Words>
-
-    @Inject
-    lateinit var wordsAdapter: WordsAdapter
+    private lateinit var wordsAdapter: WordsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +37,8 @@ class CardsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+
         checkItems()
 
         binding.searchViewWords.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
@@ -56,7 +56,6 @@ class CardsFragment : Fragment() {
 
     private fun filterList(text: String?) {
         if (text.isNullOrEmpty()) {
-            // If the query is null or empty, show all items
             wordsAdapter.differ.submitList(listWords)
         } else {
             val filteredList = listWords.filter { word ->
@@ -77,12 +76,28 @@ class CardsFragment : Fragment() {
             CoroutineScope(Dispatchers.Main).launch {
                 listWords = list
                 wordsAdapter.differ.submitList(list)
-                setupRecyclerView()
             }
         }
     }
 
     private fun setupRecyclerView() {
+        wordsAdapter = WordsAdapter { word ->
+            val bundle = Bundle().apply {
+                putInt("wordsId", word.wordsId)
+                putString("sid", word.sid)
+                putString("sourceCode", word.sourceLangCode)
+                putString("targetCode", word.targetLangCode)
+            }
+
+            val updateCardFragment = UpdateCardFragment().apply {
+                arguments = bundle
+            }
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, updateCardFragment)
+                .addToBackStack(null)
+                .commit()
+        }
         binding.recyclerViewWords.apply {
             layoutManager = LinearLayoutManager(activity)
             adapter = wordsAdapter
