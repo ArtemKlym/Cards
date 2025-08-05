38d3aad582f1e81fc882dae196645287e6126653
Artem <artemklymenko892@gmail.com>
2025-08-05 06:53:08 +0300

    Create WordsViewModelTest

--- app/src/test/java/com/artemklymenko/cards/utils/FakeWordsList.kt
+++ app/src/test/java/com/artemklymenko/cards/utils/FakeWordsList.kt
@@ -69,7 +69,60 @@ fun getFakeWord(): Words {
         lastSeen = 5L,
         sourceLangCode = "en",
         targetLangCode = "uk",
-        sid = "kmfkdmb",
+        sid = "iooeorepor",
         sync = 9L,
+    )
+}
+
+fun getFakeWordsList(): List<Words> {
+    return listOf(
+        Words(
+            wordsId = 0,
+            origin = "hello",
+            translated = "привіт",
+            exampleOfUse = "",
+            priority = 16,
+            lastSeen = 5L,
+            sourceLangCode = "en",
+            targetLangCode = "uk",
+            sid = "pninknm",
+            sync = 9L,
+        ),
+        Words(
+            wordsId = 1,
+            origin = "run down",
+            translated = "виснажуватися",
+            exampleOfUse = "I feel run down after working all week",
+            priority = 6,
+            lastSeen = 5L,
+            sourceLangCode = "en",
+            targetLangCode = "uk",
+            sid = "mbvcnmgkj",
+            sync = 9L,
+        ),
+        Words(
+            wordsId = 2,
+            origin = "conducive",
+            translated = "що сприяють",
+            exampleOfUse = "making a certain situation or outcome",
+            priority = 16,
+            lastSeen = 5L,
+            sourceLangCode = "en",
+            targetLangCode = "uk",
+            sid = "kmfkdmb",
+            sync = 9L,
+        ),
+        Words(
+            wordsId = 4,
+            origin = "apart from",
+            translated = "окремо від",
+            exampleOfUse = "besides",
+            priority = 6,
+            lastSeen = 3008000001L,
+            sourceLangCode = "en",
+            targetLangCode = "uk",
+            sid = "ewqrtb",
+            sync = 0L,
+        )
     )
 }
